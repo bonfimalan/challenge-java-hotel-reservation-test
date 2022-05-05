1d8c55a14dfb8aa101496f180b7f2be13a54cc63
Alan Bonfim Santos <alanbonfim2000@gmail.com>
2022-05-05 20:53:31 -0300

    3 tests added

--- src/test/java/FailingTest.java
+++ src/test/java/FailingTest.java
@@ -17,5 +17,19 @@ public class FailingTest {
        HotelReservation Hotel = new HotelReservation();
        assertEquals("Ridgewood", Hotel.getCheapestHotel("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"));
    }
-   
+   @Test
+   public void test4() {
+       HotelReservation Hotel = new HotelReservation();
+       assertEquals("Lakewood", Hotel.getCheapestHotel("Regular: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat), 28Mar2009(sun), 29Mar2009(mon), 30Mar2009(tues)"));
+   }
+   @Test
+   public void test5() {
+       HotelReservation Hotel = new HotelReservation();
+       assertEquals("Lakewood", Hotel.getCheapestHotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed), 19Mar2009(thur)"));
+   }
+   @Test
+   public void test6() {
+       HotelReservation Hotel = new HotelReservation();
+       assertEquals("Lakewood", Hotel.getCheapestHotel("Rewards: 25Mar2009(wed), 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"));
+   }
 }
